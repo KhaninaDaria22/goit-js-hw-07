b52faf4e7fb5127f51c78f2eb71fc0a0c26a3373
KhaninaDaria22 <khanina_dasha@ukr.net>
2023-09-21 00:06:58 +0300

    commit 1

---
+++ js/01-gallery.js
@@ -0,0 +1,51 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryList = document.querySelector(".gallery");
+
+
+const markup = galleryItems.map(({ description, original, preview }) => {
+    return `<li class="gallery__item js-product-item">
+          <a class="gallery_link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </li>`
+  }).join('');
+
+galleryList.insertAdjacentHTML('beforeEnd', markup);
+galleryList.addEventListener('click', openGaleryClick);
+
+
+function openGaleryClick(event) {
+event.preventDefault();
+
+    if (event.target === event.currentTarget) {
+        return;
+    }
+    // const currentGalery = event.target.closest(".js-product-item");
+    // console.log(currentGalery);
+
+    const imgDataset = event.target.dataset.source;
+    const imgDescription = event.alt;
+
+    const instance = basicLightbox.create (`
+    <img src="${imgDataset}" alt="${imgDescription}"/>
+    `,  { onClose: () => { document.removeEventListener('keydown', onEscKeydown); } })
+
+    instance.show()
+
+        document.addEventListener('keydown', onEscKeydown);
+
+        function onEscKeydown(event) {
+          if (event.code === 'Escape') {
+            instance.close();
+          }
+}
+}
+
+// console.log(galleryItems);
